44eac06aa93e30cba3244da16ef8ef3f58bc8712
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2021-05-06 15:53:04 -0300

    Incluindo Linq

---
+++ Linq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Query onde sera listado todos os arquivos do diretorio
+            // Sendo atribuido o caminho completo
+            var query1 = from arquivo in Directory.GetFiles(@"c:\windows")
+                         select arquivo;
+
+            foreach (var arq in query1)
+            {
+                Console.Write($"\n{arq}");
+
+            }
+
+
+        }
+    }
+}
